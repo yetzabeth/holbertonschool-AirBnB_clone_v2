4643e38c6493c18403852f86de47fff564d7d67d
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-20 12:54:53 -0300

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,3 +7,4 @@ CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* to 'hbnb_dev'@'localhost';
 -- Grant SELECT privilege on performance_schema
 GRANT SELECT ON performance_schema.* to ' hbnb_dev'@'localhost';
+SHOW DATABASES;
