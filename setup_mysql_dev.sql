d89f57e476ce27beab4babfac08fcd0bec80ca55
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-20 13:25:39 -0300

    update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 --  script that prepares a MySQL server
 CREATE DATABASE IF NOT EXIST hbnb_dev_db;
-USE hbnb_dev_db;
 -- Crea o usa un usuario existente en hbnb_dev en el localhost
 CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on hbnb_dev_db
