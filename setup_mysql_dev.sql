5bcc5b3001e93a7640634347855b2206e06850de
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-20 12:48:02 -0300

    update

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+--  script that prepares a MySQL server
+CREATE DATABASE IF NOT EXIST hbnb_dev_db;
+USE hbnb_dev_db;
+-- Crea o usa un usuario existente en hbnb_dev en el localhost
+CREATE USER IF NOT EXIST 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* to 'hbnb_dev'@'localhost';
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* to ' hbnb_dev'@'localhost';
