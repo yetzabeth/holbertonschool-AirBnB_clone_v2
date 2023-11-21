e3d16c3b1cd66848a4969dd116081fb8727a648e
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-21 15:45:44 -0300

    update

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,7 @@
 
 from os import getenv
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
-if HBNB_TYPE_STORAGE == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
--- models/state.py
+++ models/state.py
@@ -9,11 +9,24 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
-
-class State(BaseModel):
+class State(BaseModel, Base):
+    """
+    State class
+    """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            storage
+            """
+            city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
