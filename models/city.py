dadb83f7e52cc12f2ee5e35ac724dc416511f3e9
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-11-21 15:55:50 -0300

    update

--- models/city.py
+++ models/city.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """
-    The city class, contains state ID and name
-    """
-    __tablename__ = 'cities'
-    if getenv('HBNB_TYPE_STORAGE') == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship(
-            'Place',
-            backref='cities',
-            cascade='delete'
-        )
-    else:
-        name = state_id = ""
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,32 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-from os import getenv
-from models.city import City
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """
-    State class
-    """
-    __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """
-            storage
-            """
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+class State(BaseModel):
+    """ State class """
+    name = ""
