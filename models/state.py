2353dd7d9a50dff03d5f6ea8fc57ee2dbd76df38
Romi <romi@192.168.1.17>
2023-11-26 21:46:24 -0300

    update

--- models/state.py
+++ models/state.py
@@ -12,10 +12,11 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    
+
     # For DBStorage
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship
+        ("City", backref="state", cascade="all, delete-orphan")
 
     # For FileStorage
     if getenv('HBNB_TYPE_STORAGE') == 'file':
