6657cc6133d9461e7470580f1c551f27cb74c236
Romi <romi@192.168.1.17>
2023-11-26 22:26:30 -0300

    correccion en state.py

--- models/state.py
+++ models/state.py
@@ -15,8 +15,7 @@ class State(BaseModel, Base):
 
     # For DBStorage
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship
-        ("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     # For FileStorage
     if getenv('HBNB_TYPE_STORAGE') == 'file':
