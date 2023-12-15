09949035d6f2b438e6cca3f535f8f0c500a8a11d
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2023-12-15 18:49:32 +0000

    tansk 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ first end point """
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ second  end point """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ display “C ” followed by the value of the text variable """
+    text = text.replace("_", " ")
+    return f'C {text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
